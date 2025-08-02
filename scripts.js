42e4f68012c6da943bff3760142e54037d6c1d6f
Lord-ace <emmanuelashinze612@gmail.com>
2025-08-02 15:55:56 +0100

    added photosfrom coudinary

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 const play = document.getElementById('play')
 const song = document.getElementById('song')
-const body = document.getElementById('body')
+const body = document.getElementById('gallery')
 
+let imgCount = 0
+// Create 100 images with the specified source and class
+while (imgCount < 52) {
+  imgCount++
+  const img = document.createElement('img')
+  img.src = `https://res.cloudinary.com/dvjmqedze/image/upload/${imgCount}.png`
+  img.classList.add('gallery')
+  body.appendChild(img)
+}
 play.addEventListener('click', function(){
   if (song.paused){
     song.play()
@@ -11,8 +20,6 @@ play.addEventListener('click', function(){
     song.pause()
   }
 })
-const imgCount = 0
-
 })
 // https://res.cloudinary.com/dvjmqedze/image/upload/twelve.png
 
