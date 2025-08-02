73dec553e63bab6e177c5096fd76c11d8e4a9fd1
Lord-ace <emmanuelashinze612@gmail.com>
2025-08-02 19:58:51 +0100

    optimized for quickload

--- scripts.js
+++ scripts.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
 const play = document.getElementById('play')
 const song = document.getElementById('song')
 const body = document.getElementById('gallery')
+// Ensure the song is paused initially
+
+// Add an event listener to the play button
+if (play) {
+  play.addEventListener('mouseover', function() {
+    play.style.cursor = 'pointer'
+    console.log('Mouse over play button')
+    play.addEventListener('click', function(){
+      if (song.paused){
+        song.play()
+        song.volume = 0.2
+      } else{
+        song.pause()
+      }
+    })
+  })
+}
 
 let imgCount = 0
 // Create 100 images with the specified source and class
@@ -12,14 +29,4 @@ while (imgCount <= 52) {
   img.classList.add('gallery')
   body.appendChild(img)
 }
-play.addEventListener('click', function(){
-  if (song.paused){
-    song.play()
-    song.volume = 0.2
-  } else{
-    song.pause()
-  }
 })
-})
-// https://res.cloudinary.com/dvjmqedze/image/upload/twelve.png
-
